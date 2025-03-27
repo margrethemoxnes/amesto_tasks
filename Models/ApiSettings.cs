3bc1006799a47f9e602a727441277c99b0effc8f
Margrethe Moxnes <margrethe.moxnes@egde.no>
2025-03-27 20:25:14 +0100

    Remove unused model

--- Models/ApiSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace AmestoCandidateTask.Models
-{
-    public class Urls
-    {
-        public required string CompaniesUrl { get; set; }
-        public required string ProductsUrl { get; set; }
-        public required string SalesOrdersUrl { get; set; }
-    }
-}
